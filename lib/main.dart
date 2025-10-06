daf016ad27b97ed453dfaadd7b20fba5f8a8fe8c
sharmisravs-flutter <sharmisravs@gmail.com>
2025-10-06 17:01:33 +0530

    Made changes in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chatbox/presentation/pages/onboarding_page.dart';
 import 'package:chatbox/presentation/pages/splash_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
