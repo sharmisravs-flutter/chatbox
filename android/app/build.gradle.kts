2b80fc6cca15669c5ae323e850a5b1407bc149f0
sharmisravs-flutter <sharmisravs@gmail.com>
2025-10-06 16:37:15 +0530

    Firebase-initialization

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,13 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+dependencies {
+  implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
 }
 
 android {
